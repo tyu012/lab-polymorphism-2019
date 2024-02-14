a8d5d65b084dc7decf972100912fcc0d100b1fd6
Tim Yu <yuyu.tim@gmail.com>
2024-02-13 23:06:36 -0600

    Implemented tests for BottomJustified

---
+++ src/lab/polymorphism/BottomJustifiedTest.java
@@ -0,0 +1,65 @@
+package lab.polymorphism;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.abort;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for the BottomJustified text block class
+ * @author Tim Yu
+ */
+public class BottomJustifiedTest {
+  TextBlock helloWorld;
+  TextBlock empty;
+  
+  public BottomJustifiedTest() throws Exception {
+    helloWorld = new TextLine("Hello World");
+    empty = new TextLine("");
+  }
+
+  private String spaces(int n) {
+    return " ".repeat(n);
+  }
+
+  @Test
+  public void testBottomJustifyString() throws Exception {
+    for (int i = 1; i < 20; i++) {
+      TextBlock bottomJustifiedHelloWorld = new BottomJustified(helloWorld, i);
+      assertEquals(helloWorld.width(), bottomJustifiedHelloWorld.width(),
+          "bottom justifying preserves width");
+      for (int j = 0; j < i - 1; j++) {
+        assertEquals(spaces(helloWorld.width()), bottomJustifiedHelloWorld.row(j),
+            "bottom-justified padding is all-spaces");
+      }
+      assertEquals(helloWorld.row(0), bottomJustifiedHelloWorld.row(i - 1),
+          "bottom-most row equal to 'Hello World'");
+    }
+  }
+
+  @Test
+  public void testBottomJustifyBoxedString() throws Exception {
+    for (int i = 1; i < 20; i++) {
+      TextBlock bottomJustifiedBoxedHelloWorld = new BoxedBlock(new BottomJustified(helloWorld, i));
+      assertEquals(helloWorld.width() + 2, bottomJustifiedBoxedHelloWorld.width(),
+          "bottom justifying inside box preserves width");
+      for (int j = 1; j < i; j++) {
+        assertEquals("|" + spaces(helloWorld.width()) + "|", bottomJustifiedBoxedHelloWorld.row(j),
+            "bottom-justified padding is all-spaces surrounded by box");
+      }
+      assertEquals("|" + helloWorld.row(0) + "|", bottomJustifiedBoxedHelloWorld.row(i),
+          "bottom-most row equal to '|Hello World|'");
+    }
+  }
+
+  @Test
+  public void testBottomJustifyEmpty() throws Exception {
+    for (int i = 1; i < 20; i++) {
+      TextBlock bottomJustifiedEmpty = new BottomJustified(empty, i);
+      assertEquals(0, bottomJustifiedEmpty.width(), "width is 0");
+      for (int j = 0; j < i; j++) {
+        assertEquals("", bottomJustifiedEmpty.row(j), "row is empty string");
+      }
+    }
+  }
+}
