076b0a352ad6a6b6bae9ad2c67375b0f5f3b116f
DAVID_RHOADES <RHOADESD@grinnell.edu>
2024-02-13 22:31:34 -0600

    completed and documented adge case testing for eq and eqv in seperate Test.java files

--- src/lab/polymorphism/EqTest.java
+++ src/lab/polymorphism/EqTest.java
@@ -1,3 +1,10 @@
+/**
+ * Testing for the eq method in TBUtils.java
+ * @author David Rhoades
+ * @author Alyssa Trapp
+ * @author Tim Yu
+ */
+
 package lab.polymorphism;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -6,9 +13,18 @@ import static org.junit.jupiter.api.Assumptions.abort;
 import org.junit.jupiter.api.Test;
 
 public class EqTest {
-  TextBlock helloWorld = new TextLine("Hello World");
-  TextBlock helloWorldRef = helloWorld;
+  TextBlock helloworldTrunc; 
+  
+
+  public EqTest() throws Exception {
+    TextBlock helloworldTrunc = new Truncated(new TextLine("Hello World1"), 11);
+  }
+
   TextBlock helloWorldCopy = new TextLine("Hello World");
+  TextBlock helloWorld = new TextLine("Hello World");
+  TextLine empty = new TextLine("");
+  TextLine empty2 = new TextLine("");
+  TextBlock helloWorldRef = helloWorld;
 
   @Test
   public void testEqPositiveResult() {
@@ -22,6 +38,16 @@ public class EqTest {
 
   @Test
   public void testEqDifferentTextBlockInMem() {
-    assertEquals(true, TBUtils.eq(helloWorld, helloWorldCopy), "eq with different text block in memory");
+    assertEquals(false, TBUtils.eq(helloWorld, helloWorldCopy), "eq with different text block in memory");
+  }
+
+  @Test
+  public void testEmptyStrings() {
+    assertEquals(false, TBUtils.eq(empty, empty2), "eq with two different empty strings");
+  }
+
+  @Test
+  public void testLookSame() {
+    assertEquals(false, TBUtils.eq(helloWorld, helloworldTrunc), "eq look the same but built different");
   }
 }
